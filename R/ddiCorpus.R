3c244295d69396c0e0c019a9522dcd6af5f673d0
Gustavo Bruges <gbruges@gmail.com>
2020-07-15 11:05:58 -0500

    Initial commit

---
+++ R/ddiCorpus.R
@@ -0,0 +1,21 @@
+#' @importFrom tibble tibble
+NULL
+
+#' @title ddi CorpusR
+#' @description A corpus annotated with drugdrug interaction.This dataset has been created by Hulat Group at University Carlos III of Madrid.
+#'              Contact address: isegura@inf.uc3m.es or isegurabe@gmail.com
+#' @docType package
+#' @name ddicorpusR
+#' @aliases ddicorpusR-package
+
+
+
+#' @source \url{http://labda.inf.uc3m.es/DrugDDI/DrugDDI.html}
+#' @format A tibble data with two variables: \code{drug_name}, \code{text}
+"ddiCorpus"
+#' ddiCorpus
+#'
+#' A data set containing the ddiCorpus
+#' annotated with drug-drug interactions by ISABEL SEGURA-BEDMAR
+#'
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# ddicorpusR
+ A corpus annotated with drugdrug interaction.This dataset has been created by Hulat Group at University Carlos III of Madrid
